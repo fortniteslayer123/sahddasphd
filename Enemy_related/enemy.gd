8ace6ed4ab2b291f55e1c4e158022e78e4d2883c
fortniteslayer123 <chanrow01@gmail.com>
2025-11-04 13:36:33 +0200

    3 20 init

--- Enemy_related/enemy.gd
+++ Enemy_related/enemy.gd
@@ -2,15 +2,16 @@ extends CharacterBody2D
 
 @export var player_reference : CharacterBody2D
 var direction : Vector2
-var speed : float = 75
+var speed : float = 80
 var damage : float 
 var knockback : Vector2
+var separation : float
 var elite : bool = false:
 	set(value):
 		elite = value
 		if value:
 			$Sprite2D.material = load("res://Shaders/Rainbow.tres")
-			scale = Vector2(0.3,0.3)
+			scale = Vector2(0.4,0.4)
 
 var type : Enemy:
 	set(value):
@@ -20,16 +21,28 @@ var type : Enemy:
 
 #for optimization eli ei laga noobeille
 func _physics_process(delta: float) -> void:
-	var separation = (player_reference.position - position).length()
-	if separation >= 1700 and not elite:
-		
+	check_separation(delta)
+	knockback_update(delta)
+
+	
+func check_separation(_delta):
+	separation = (player_reference.position - position).length()
+	if separation >= 1500 and not elite:
 		queue_free()
+		
+		
+	if separation < player_reference.nearest_enemy_distance:
+		player_reference.nearest_enemy = self
+		
+		
+		
+
 	
 	
+func knockback_update(delta):
 	velocity = (player_reference.position - position).normalized() * speed
 	knockback = knockback.move_toward(Vector2.ZERO, 1) 
 	velocity += knockback
-	
 	var collider = move_and_collide(velocity * delta)
 	if collider:
 		collider.get_collider().knockback = (collider.get_collider().global_position - global_position).normalized() * 50
--- Player_Related/player.gd
+++ Player_Related/player.gd
@@ -2,12 +2,24 @@ extends CharacterBody2D
 
 
 var speed : float = 150
-var health : float = 100:
+var health : float = 150:
 	set(value):
 		health = value
 		%Health.value = value
 
+var nearest_enemy : CharacterBody2D
+var nearest_enemy_distance : float = INF
+
+
 func _physics_process(delta: float) -> void:
+	if nearest_enemy:
+		nearest_enemy_distance = nearest_enemy.separation
+		print(nearest_enemy.name)
+	else:
+		nearest_enemy_distance = INF
+	
+	
+	
 	velocity = Input.get_vector("left", "right", "up", "down") * speed
 	move_and_collide(velocity * delta)
 
